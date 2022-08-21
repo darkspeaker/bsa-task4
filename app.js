36b94b78f3d96a0abf4f3eedc0d9d2ae126d491c
darkspeaker <graggg13@gmail.com>
2022-08-21 20:11:32 +0300

    feat: done 4 task

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-// todo: add logic
+const container = document.querySelector('.container'),
+      allSlides = container.querySelectorAll('.slide')
+
+allSlides.forEach(slide => {
+  slide.addEventListener('click', (e) => {
+    removePreviousActiveElement(allSlides)
+    setActiveElement(e.target)
+  })
+})
+
+function setActiveElement(element){
+  element.classList.add('active')
+}
+
+function removePreviousActiveElement(listElements){
+  listElements.forEach(element =>{
+    element.classList.remove('active')
+  })
+}
